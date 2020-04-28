cd0508bf4bfa660be42ab7fd3b1c1075362508fd
jdeng <jdeng@axs.com>
2020-04-28 14:53:10 -0700

    change  broker calculation to intermediate step

--- models/customers.sql
+++ models/customers.sql
@@ -10,13 +10,15 @@ customer_orders as (
         customer_unique_id,
         min(sale_datetime) as first_order_date,
         max(sale_datetime) as most_recent_order_date,
-
-        COUNT(DISTINCT CASE WHEN (NOT COALESCE(pricing_mode_id = 1 , FALSE)) THEN order_ticket_unique_id ELSE NULL END) AS tickets_sold_no_comps,
+        COUNT(DISTINCT CASE WHEN (NOT COALESCE(pricing_mode_id = 1 , FALSE)) THEN 
+        order_ticket_unique_id ELSE NULL END) AS tickets_sold_no_comps,
         COUNT(DISTINCT order_ticket_unique_id) AS number_of_tickets_sold,
         COUNT(DISTINCT order_unique_id) AS number_of_orders,
         SUM(DISTINCT amount_gross) AS total_revenue,
-        COUNT(DISTINCT CASE WHEN (ticket_state = 'TRANSFERRED') THEN ticket_id ELSE NULL END) AS count_transferred_tickets,
-        COUNT(DISTINCT CASE WHEN (ticket_state = 'TRANSFERRED') THEN transfer_action_id || ':' || ticket_id  ELSE NULL END) AS count_transfers
+        COUNT(DISTINCT CASE WHEN (ticket_state = 'TRANSFERRED') THEN 
+        ticket_id ELSE NULL END) AS count_transferred_tickets,
+        COUNT(DISTINCT CASE WHEN (ticket_state = 'TRANSFERRED') THEN 
+        transfer_action_id || ':' || ticket_id  ELSE NULL END) AS count_transfers
 
     from order_flash
     group by 1
@@ -25,9 +27,9 @@ final as (
     select
         customers.customer_unique_id,
         customers.email,
+        customers.is_broker,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        CASE WHEN broker_email is not null THEN 1 ELSE 0 END AS is_broker,
         coalesce(customer_orders.tickets_sold_no_comps, 0) as tickets_sold_no_comps,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         coalesce(customer_orders.number_of_tickets_sold, 0) as number_of_tickets_sold,
