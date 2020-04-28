b534eb3870c39eda365f1361223b8d9e6de56f75
jdeng <jdeng@axs.com>
2020-04-27 18:02:18 -0700

    include additional dimension in model

--- models/customers.sql
+++ models/customers.sql
@@ -4,14 +4,28 @@ with customers as (
 order_tickets as (
     select * from {{ ref('stg_orders_aggregate')}}
 ),
+
 customer_orders as (
     select
         customer_unique_id,
         min(sale_datetime) as first_order_date,
         max(sale_datetime) as most_recent_order_date,
-        COUNT(order_unique_id) as number_of_orders,
-        COUNT(order_ticket_unique_id) AS tickets_purchased,
-        SUM(amount_gross) AS total_revenue
+        
+        -- COUNT(DISTINCT CASE WHEN (NOT COALESCE(is_canceled , FALSE)) AND 
+        -- (NOT COALESCE(pricing_mode_id = 1 , FALSE)) 
+        -- THEN order_ticket_unique_id ELSE NULL END) AS tickets_sold_no_comps,
+        -- COUNT(DISTINCT CASE WHEN NOT COALESCE(is_canceled , FALSE) 
+        -- THEN order_ticket_unique_id ELSE NULL END) AS number_of_tickets_sold,
+        -- COUNT(DISTINCT CASE WHEN NOT COALESCE(is_canceled , FALSE) 
+        -- THEN order_unique_id ELSE NULL END) AS number_of_orders,
+        -- SUM(DISTINCT CASE WHEN NOT COALESCE(is_canceled , FALSE) 
+        -- THEN amount_gross ELSE NULL END) AS total_revenue
+        COUNT(DISTINCT CASE WHEN (NOT COALESCE(pricing_mode_id = 1 , FALSE)) 
+        THEN order_ticket_unique_id ELSE NULL END) AS tickets_sold_no_comps,
+        COUNT(DISTINCT order_ticket_unique_id) AS number_of_tickets_sold,
+        COUNT(DISTINCT order_unique_id) AS number_of_orders,
+        SUM(DISTINCT amount_gross) AS total_revenue
+
     from order_tickets
     group by 1
 ),
@@ -21,9 +35,10 @@ final as (
         customers.email,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        coalesce(customer_orders.tickets_sold_no_comps, 0) as tickets_sold_no_comps,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(customer_orders.tickets_purchased, 0) as ticket_purchased,
-        customer_orders.total_revenue
+        coalesce(customer_orders.number_of_tickets_sold, 0) as number_of_tickets_sold,
+        coalesce(customer_orders.total_revenue, 0) as total_revenue
     from customers
     left join customer_orders using (customer_unique_id)
 )
--- models/staging/stg_orders_aggregate.sql
+++ models/staging/stg_orders_aggregate.sql
@@ -5,7 +5,9 @@ select
     amount_gross,
     sale_datetime,
     zone_unique_id,
-    price_code_unique_id,
+    pricing_mode_id,
     seat_unique_id,
     is_canceled
 from ticketing.order_tickets
+INNER JOIN ticketing.price_codes USING(price_code_unique_id)
+WHERE is_canceled is False
